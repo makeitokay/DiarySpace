e8dbdc714e823f8fc82fe16f80d7ecfa2d238a71
javabird25 <shockck84@gmail.com>
2020-04-07 19:02:49 +0500

    Исправил сборку данных с чекбоксов

--- diaryspace/schools/widgets.py
+++ diaryspace/schools/widgets.py
@@ -4,3 +4,7 @@ from django.forms import widgets
 class BootstrapCheckboxSelectMultiple(widgets.CheckboxSelectMultiple):
     template_name = "schools/widgets/bootstrap_checkbox_select_multiple.html"
     option_template_name = "schools/widgets/bootstrap_checkbox_select_multiple_option.html"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.attrs["class"] = self.attrs.get("class", "") + " custom-control-input"
